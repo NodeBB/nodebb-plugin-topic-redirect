f7c0930540c3aeda3d872558ffe06cfc9e00614d
Julian Lam <julian@nodebb.org>
2021-09-10 13:11:55 -0400

    fix: crash if no redirects are present

--- library.js
+++ library.js
@@ -27,14 +27,16 @@ plugin.addAdminNavigation = (header) => {
 plugin.handleTopicRedirect = async ({ req, res }) => {
 	if (['/api/topic', '/topic'].some(prefix => req.url.startsWith(prefix))) {
 		let { redirects } = await meta.settings.get('topic-redirect');
-		redirects = redirects.reduce((memo, cur) => {
-			memo.set(parseInt(cur.tid, 10), cur);
-			return memo;
-		}, new Map());
+		if (redirects && redirects.length) {
+			redirects = redirects.reduce((memo, cur) => {
+				memo.set(parseInt(cur.tid, 10), cur);
+				return memo;
+			}, new Map());
 
-		if (redirects.has(parseInt(req.params.topic_id, 10))) {
-			const redirect = redirects.get(parseInt(req.params.topic_id, 10));
-			controllerHelpers.redirect(res, redirect.url, redirect.type === 308);	// 307 for everything else
+			if (redirects.has(parseInt(req.params.topic_id, 10))) {
+				const redirect = redirects.get(parseInt(req.params.topic_id, 10));
+				controllerHelpers.redirect(res, redirect.url, redirect.type === 308);	// 307 for everything else
+			}
 		}
 	}
 };
